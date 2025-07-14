33407f1e80b01bfc3db817c3b2e40f72ce74a770
宮澤昭海 <>
2025-07-15 08:16:05 +0900

    crud.pyをcrud配下のファイルに分割

---
+++ app/crud/room.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models import models
+from app.schemas import schemas
+
+# 会議室一覧取得
+def get_rooms(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Room).offset(skip).limit(limit).all()
+
+# 会議室登録
+def create_room(db: Session, room: schemas.Room):
+    db_room = models.Room(room_name=room.room_name, capacity=room.capacity)
+    db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
---
+++ app/crud/user.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import Session
+from app.models import models
+from app.schemas import schemas
+
+# ユーザー一覧取得
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+# ユーザー登録
+def create_user(db: Session, user: schemas.User):
+    db_user = models.User(username=user.username)
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
