aa0314254eee41fc95df10412b2f12f1ce71a903
宮澤昭海 <>
2025-07-24 08:06:26 +0900

    room更新処理追加

--- app/api/room.py
+++ app/api/room.py
@@ -3,7 +3,9 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.db.database import get_db
-from app.crud.room import get_rooms, create_room as create_room_logic
+from app.crud.room import get_rooms
+from app.crud.room import create_room as create_room_logic
+from app.crud.room import update_room as update_room_logic
 from app.schemas.room import Room, RoomCreate
 
 router = APIRouter()
@@ -18,3 +20,9 @@ async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 @router.post("/rooms", response_model=Room)
 async def create_room(room: RoomCreate, db: Session = Depends(get_db)):
     return create_room_logic(db=db, room=room)
+
+# ユーザー更新
+@router.put("/rooms/{room_id}", response_model=Room)
+async def update_room(room_id: int, room: RoomCreate, db: Session = Depends(get_db)):
+    print(f"Updating room with ID: {room_id} and data: {room}")
+    return update_room_logic(db=db, room=room, room_id=room_id)
--- app/crud/room.py
+++ app/crud/room.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from app.models.room import Room
 from app.schemas.room import Room as schemas_Room
@@ -10,6 +11,18 @@ def get_rooms(db: Session, skip: int = 0, limit: int = 100):
 def create_room(db: Session, room: schemas_Room):
     db_room = Room(room_name=room.room_name, capacity=room.capacity)
     db.add(db_room)
+    db.commit()
+    db.refresh(db_room)
+    return db_room
+
+# 会議室更新
+def update_room(db: Session, room: schemas_Room, room_id: int):
+    db_room = db.query(Room).filter(Room.room_id == room_id).first()
+    if not db_room:
+        raise HTTPException(status_code=404, detail="Room not found")
+
+    db_room.room_name = room.room_name
+    db_room.capacity = room.capacity
     db.commit()
     db.refresh(db_room)
     return db_room
