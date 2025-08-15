512b9b3823464f6d8ada2bdf7e42107e2f9bd495
宮澤昭海 <>
2025-08-15 21:20:01 +0900

    会議室の登録、更新、削除に管理者権限確認処理追加

--- app/api/room.py
+++ app/api/room.py
@@ -9,6 +9,8 @@ from app.crud.room import create_room as create_room_logic
 from app.crud.room import update_room as update_room_logic
 from app.crud.room import delete_room as delete_room_logic
 from app.schemas.room import Room, RoomCreate
+from app.schemas.user import User
+from app.core.deps import admin_required
 
 router = APIRouter()
 
@@ -20,17 +22,17 @@ async def read_rooms(skip: int = 0, limit: int = 100, db: Session = Depends(get_
 
 # 会議室登録
 @router.post("/rooms", response_model=Room)
-async def create_room(room: RoomCreate, db: Session = Depends(get_db)):
+async def create_room(room: RoomCreate, current_user: User = Depends(admin_required), db: Session = Depends(get_db)):
     return create_room_logic(db=db, room=room)
 
 # 会議室更新
 @router.put("/rooms/{room_id}", response_model=Room)
-async def update_room(room_id: int, room: RoomCreate, db: Session = Depends(get_db)):
+async def update_room(room_id: int, room: RoomCreate, current_user: User = Depends(admin_required), db: Session = Depends(get_db)):
     print(f"Updating room with ID: {room_id} and data: {room}")
     return update_room_logic(db=db, room=room, room_id=room_id)
 
 # 会議室削除
 @router.delete("/rooms/{room_id}")
-async def delete_room(room_id: int, db: Session = Depends(get_db)):
+async def delete_room(room_id: int, current_user: User = Depends(admin_required), db: Session = Depends(get_db)):
     delete_room = delete_room_logic(db=db, room_id=room_id)
     return JSONResponse(content={"message": f"{delete_room.room_name} を削除しました"})
